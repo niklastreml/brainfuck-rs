d6bf4b3fb3a64317001611c8f2cd731536bc94e9
Niklas Treml <treml.niklas@gmail.com>
2023-07-04 21:52:59 +0200

    feat: remove unnecessary loop

--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,30 @@ pub fn brainfuck(item: TokenStream) -> TokenStream {
     let mut output = "".to_string();
     let mut buffer_size = 0;
 
-    for t in item {
-        let s = format!("{}", t);
-        let mut s = s.chars();
-        s.next();
-        s.next_back();
-        let s = s.as_str();
+    let t = item.into_iter().next().expect("Expected &str");
+    // for t in item {
+    let s = format!("{}", t);
+    let mut s = s.chars();
+    s.next();
+    s.next_back();
+    let s = s.as_str();
 
+    if s.matches("[").count() == 0 {
         buffer_size = s.matches(">").count();
-        if buffer_size == 0 {
-            buffer_size += 1;
-        }
+    } else {
+        buffer_size = 30_000;
+    }
+    if buffer_size == 0 {
+        buffer_size += 1;
+    }
 
-        if s.matches(",").count() > 0 {
-            output.push_str("use std::io::Read;\n");
-        }
+    if s.matches(",").count() > 0 {
+        output.push_str("use std::io::Read;\n");
+    }
 
-        let mut chars_iter = s.chars().peekable();
-        while let Some(char) = chars_iter.next() {
-            match char {
+    let mut chars_iter = s.chars().peekable();
+    while let Some(char) = chars_iter.next() {
+        match char {
                 '+' => {
                     let mut amount = 1;
                     while chars_iter.next_if_eq(&'+').is_some() {
@@ -43,14 +48,14 @@ pub fn brainfuck(item: TokenStream) -> TokenStream {
                 '<' => output.push_str("ptr -= 1;\n"),
                 '.' => output.push_str("print!(\"{}\", tape[ptr] as char);\n"),
                 ',' => output.push_str(
-                    "tape[ptr] = std::io::stdin().bytes().next().and_then(|result| result.ok()).expect(\"Expected value\");\n",
+                    "tape[ptr] = std::io::stdin().bytes().next().and_then(|result| result.ok()).expect(\"Could not parse value into u8\");\n",
                 ),
                 '[' => output.push_str("while tape[ptr] != 0 {\n"),
                 ']' => output.push_str("}\n"),
                 _ => (),
             }
-        }
     }
+    // }
 
     let mut output = format!(
         "{{
